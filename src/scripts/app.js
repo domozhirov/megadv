26e360856e3f810c7293bd56b565e6457a2204e8
Domozhirov Mikhail <michael@domozhirov.ru>
2019-11-25 07:20:08 +0500

    products

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,14 +14,48 @@ $(function () {
     $navigationWrapper.pudgeJS('toggle');
   });
 
-  $('.owl-carousel').owlCarousel({
-    items: 1,
-    loop: false,
-    margin: 0,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 10000,
-    autoplayHoverPause: true
+  const $owlCarousel = $('.owl-carousel');
+  const owlCarouselCommonOptions = {};
+
+  $owlCarousel.each((index, item) => {
+    let $slider = $(item);
+    let owlCarouselOptions;
+
+    if ($slider.hasClass('owl-products')) {
+      owlCarouselOptions = Object.assign(owlCarouselCommonOptions, {
+        loop: false,
+        nav: false,
+        dots: true,
+        margin: 30,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          480: {
+            items: 2,
+          },
+          768: {
+            items: 3,
+            margin: 20,
+          },
+          1366: {
+            items: 5,
+          },
+        }
+      });
+    } else {
+      owlCarouselOptions = Object.assign(owlCarouselCommonOptions, {
+        items: 1,
+        loop: false,
+        margin: 0,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+      });
+    }
+
+    $slider.owlCarousel(owlCarouselOptions);
   });
 });
