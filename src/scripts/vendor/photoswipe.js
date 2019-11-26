4f546a9f24d49eb0b08ed587a875b7cd6f071a72
Domozhirov Mikhail <michael@domozhirov.ru>
2019-11-27 02:19:38 +0500

    added product page

---
+++ src/scripts/vendor/photoswipe.js
@@ -0,0 +1,5 @@
+import PhotoSwipe from 'photoswipe';
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
+
+window.PhotoSwipe = PhotoSwipe;
+window.PhotoSwipeUI_Default = PhotoSwipeUI_Default;
